54e58568674a999f598d18dffb0e2192a0060ef9
Sarah Bahgat <sarah.bahgat22@gmail.com>
2019-07-06 21:15:52 +0200

    'fix'

--- category_items/__init__.py
+++ category_items/__init__.py
@@ -10,8 +10,8 @@ app.config['SECRET_KEY'] = '7a96ffec9bab2f6cb18ad50d0b53fb6b'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-from category_items import routes
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+from category_items import routes
