2f763f68571aa08f65ac3ecc84c62731be2d552f
Sarah Bahgat <sarah.bahgat22@gmail.com>
2019-07-06 21:15:08 +0200

    'fix'

--- category_items/__init__.py
+++ category_items/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from category_items import routes
 
 #configuring application and database
 
@@ -11,6 +10,7 @@ app.config['SECRET_KEY'] = '7a96ffec9bab2f6cb18ad50d0b53fb6b'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+from category_items import routes
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
